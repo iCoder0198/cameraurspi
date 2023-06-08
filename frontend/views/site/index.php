88ddcf7fd30874466b16bbea1d45781fa47b71d3
Qalandar Dev <uzwebdevolopersn2@gmail.com>
2023-06-08 11:32:46 +0500

    Update index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,4 +1,3 @@
-
 <div class="accordion">
     <div class="accordion-item">
         <h2 class="accordion-header">
@@ -8,47 +7,27 @@
         </h2>
         <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne">
             <div class="accordion-body">
-                <div class="accordion">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2_1">
-                                5230100-Iqtisodiyot (tarmoqlar va sohalar bo‘yicha)
-                            </button>
-                        </h2>
-                        <div id="collapse2_1" class="accordion-collapse collapse" aria-labelledby="heading2_1">
-                            <div class="accordion-body">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Talabalar Soni: 212</li>
-                                    <li class="list-group-item">Sana: 08-09.06.2023</li>
-                                    <li class="list-group-item">Vaqt: 09:00-18:00</li>
-                                </ul>
-                                <div><iframe width="640" height="480" src="https://rtsp.me/embed/nDfD5FtN/" frameborder="0" allowfullscreen></iframe><p align="right">powered by <a href="https://rtsp.me" title ='RTSP.ME - Free website RTSP video steaming service' target="_blank" >rtsp.me</a></p></div>
-                            </div>
-                        </div>
+                <!-- Nav tabs -->
+                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="pills-camera1-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-camera1" type="button" role="tab" aria-controls="pills-camera1"
+                                aria-selected="true">Camera 1
+                        </button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-camera2-tab" data-bs-toggle="pill"
+                                data-bs-target="#pills-camera2" type="button" role="tab" aria-controls="pills-camera2"
+                                aria-selected="false">Camera 2
+                        </button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-camera1" role="tabpanel" aria-labelledby="pills-camera1-tab">
+                        <div><iframe width="640" height="480" src="https://rtsp.me/embed/nDfD5FtN/" frameborder="0" allowfullscreen></iframe><p align="right">powered by <a href="https://rtsp.me" title ='RTSP.ME - Free website RTSP video steaming service' target="_blank" >rtsp.me</a></p></div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2_2">
-                                5610200-Mehmonxona xo‘jaligini tashkil etish va boshqarish
-                            </button>
-                        </h2>
-                        <div id="collapse2_2" class="accordion-collapse collapse" aria-labelledby="heading2_2">
-                            <div class="accordion-body">
-                                <p>This is the content for nested section 1.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2_3">
-                                5610300-Turizm (faoliyat yo‘nalishlari bo‘yicha ) (rus)
-                            </button>
-                        </h2>
-                        <div id="collapse2_3" class="accordion-collapse collapse" aria-labelledby="heading2_3">
-                            <div class="accordion-body">
-                                <p>This is the content for nested section 1.</p>
-                            </div>
-                        </div>
+                    <div class="tab-pane fade" id="pills-camera2" role="tabpanel" aria-labelledby="pills-camera2-tab">
+                        <div><iframe width="640" height="480" src="https://rtsp.me/embed/TBHhGEsY/" frameborder="0" allowfullscreen></iframe><p align="right">powered by <a href="https://rtsp.me" title ='RTSP.ME - Free website RTSP video steaming service' target="_blank" >rtsp.me</a></p></div>
                     </div>
                 </div>
             </div>
